5642bc5dc40ad4e4cfd7dfd823cc5596a1aa3b3b
SEALTeamRicks <30378727+louisvarley@users.noreply.github.com>
2023-01-21 13:09:47 +0000

    new navigation

--- src/Navigation.php
+++ src/Navigation.php
@@ -3,20 +3,20 @@
 use \App\Services\FilterService as Filter;
 
 $this->page_data['nav'] = [
-		'rats' => ['title' => 'Rats', 'link' => '#', 'subitems' => [
+		'rats' => ['title' => 'Rats', 'link' => '#', 'icon' => 'fa fa-paw', 'subitems' => [
 			['type' => 'item', 'title' => 'New Rat', 'link' => '/rat/new'],
 			['type' => 'item', 'title' => 'All Rats', 'link' => '/rat/list?orderby=id&&order=desc'],	
 			['type' => 'divider'],	
 			['type' => 'item', 'title' => 'My Rats', 'link' => '/rat/list?orderby=id&&order=desc&search=louis'],			
 		]],
-		'litters' => ['title' => 'Litters', 'link' => '#', 'subitems' => [
+		'litters' => ['title' => 'Litters', 'link' => '#', 'icon' => 'fa fa-baby-carriage', 'subitems' => [
 			['type' => 'item', 'title' => 'New Litter', 'link' => '/litter/new'],
 			['type' => 'item', 'title' => 'All Litters', 'link' => '/litter/list?orderby=id&order=desc'],	
 			['type' => 'divider'],				
 			['type' => 'item', 'title' => 'My Litters', 'link' => '/litter/list?orderby=id&order=desc&search=%22for%20sale%22'],
 			
 		]],			
-		'configuration' => ['title' => 'Configuration', 'link' => '#', 'subitems' => [
+		'configuration' => ['title' => 'Configuration', 'icon' => 'fa fa-cog', 'link' => '#', 'subitems' => [
 			['type' => 'item', 'title' => 'Users', 'link' => '/user/list'],	
 			['type' => 'item', 'title' => 'Invite', 'link' => '/user/invite'],				
 			['type' => 'item', 'title' => 'Affiliations', 'link' => '/affiliation/list'],			
@@ -25,7 +25,7 @@ $this->page_data['nav'] = [
 			['type' => 'item', 'title' => 'Settings', 'link' => '/settings/edit'],	
 			['type' => 'item', 'title' => 'Properties', 'link' => '/property/list'],					
 		]],					
-		'help' => ['title' => 'Help', 'link' => '#', 'subitems' => [
+		'help' => ['title' => 'Help', 'link' => '#', 'icon' => 'fa fa-question-circle', 'subitems' => [
 			['type' => 'item', 'title' => 'Wiki', 'link' => 'https://github.com/louisvarley/PRATS/wiki'],			
 		]],	
 
